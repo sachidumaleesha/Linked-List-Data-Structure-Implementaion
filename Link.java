8f26976be248c074d23b9777fe8970800d06395a
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-02-24 13:48:33 +0000

    Implemented the Link Class

---
+++ Link.java
@@ -0,0 +1,13 @@
+class Link{
+  public int iData;
+  public Link next;
+
+  public Link(int s){
+    iData = s;
+    next = null;
+  }
+
+  public void displayLink(){
+    System.out.println(iData);
+  }
+}

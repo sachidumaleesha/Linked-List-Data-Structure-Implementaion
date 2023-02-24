38c8072957b6a5baf100d90b139f770bade11026
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-02-24 17:02:22 +0000

    Implement stack with the help of listList Data Structure

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+// Implemet stack with the hhelp of listlist Data Structure
+
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner scanner = new Scanner(System.in);
+    linkList l = new linkList();
+
+    for (int i = 0; i < 5; i++) {
+      System.err.println("Enter a number: ");
+      l.insertFirst(scanner.nextInt());
+    }
+
+    scanner.close();
+
   }
 }
